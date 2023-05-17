26407ce4eb07f67626e11d1952afbf8c175518a2
Donyetta <donyettabarnes@gmail.com>
2023-05-17 01:34:23 -0400

    update capstone

--- components/views/Home.js
+++ components/views/Home.js
@@ -3,7 +3,25 @@ import myImage from "/assets/images/pexels-photo-3758435.jpeg";
 
 export default state => html`
   <section id="Home">
-    <img src="${myImage}" style="width:1450px;height:1200px" />
+    <script src="https://api.mqcdn.com/sdk/mapquest-js/v1.3.2/mapquest.js"></script>
+    <link
+      type="text/css"
+      rel="stylesheet"
+      href="https://api.mqcdn.com/sdk/mapquest-js/v1.3.2/mapquest.css"
+    />
+    <script type="text/javascript">
+      window.onload = function() {
+        L.mapquest.key = "BeevrxWLZK7u4i0jBeGstiFCs8GcA7C1";
+        let map = L.mapquest.map("map", {
+          center: [36.803449, -76.009947],
+          layers: L.mapquest.tileLayer("map"),
+          zoom: 12
+        });
+        map.addControl(L.mapquest.control());
+      };
+    </script>
+
+    <img src="${myImage}" style="width:1450px;height:600px" />
     <header>Naval Air Station Oceana Visitor Control Center</header>
     <h1>Contact Info:</h1>
     <p>
@@ -12,19 +30,6 @@ export default state => html`
       Virginia Beach, VA <br />
       (757)-433-3212
     </p>
-
-    <div src="https://api.mqcdn.com/sdk/mapquest-js/v1.3.2/mapquest.js">
-      <link
-        type="text/css"
-        rel="stylesheet"
-        href="https://api.mqcdn.com/sdk/mapquest-js/v1.3.2/mapquest.css"
-      />
-
-      window.onload = function() { L.mapquest.key =
-      'BeevrxWLZK7u4i0jBeGstiFCs8GcA7C1'; let map = L.mapquest.map('map', {
-      center: [36.803449, -76.009947], layers: L.mapquest.tileLayer('map'),
-      zoom: 12 }); map.addControl(L.mapquest.control()); }
-    </div>
 
     <h2>
       Directions From Interstate 64 East or West: <br />
